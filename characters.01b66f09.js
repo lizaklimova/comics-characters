7a33022c2e5cd6589265540d2813e14ce890dc56
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2024-02-07 12:36:43 +0000

    Deploying to gh-pages from @ lizaklimova/comics-characters@4ae7c9af8908efbfa2c8204a8eaca896cb29701f 🚀

--- characters.01b66f09.js
+++
@@ -1,2 +0,0 @@
-!function(){document.addEventListener("DOMContentLoaded",(function(){var e=document.getElementById("sticky-header");window.addEventListener("scroll",(function(){window.scrollY>50?e.classList.add("solid_header"):e.classList.remove("solid_header")}))}));var e=(new Date).toISOString().split("T")[0];document.querySelector("#date").max=e}();
-//# sourceMappingURL=characters.01b66f09.js.map
--- characters.8c53e35e.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(()=>{const e=document.getElementById("sticky-header");window.addEventListener("scroll",(()=>{window.scrollY>50?e.classList.add("solid_header"):e.classList.remove("solid_header")}))}));const e=(new Date).toISOString().split("T")[0];document.querySelector("#date").max=e;
-//# sourceMappingURL=characters.8c53e35e.js.map
